3db81367107fc90442a23908b5851dfa8e14ee7b
jimmy-arch <71256968+jimmy-arch@users.noreply.github.com>
2021-03-17 17:44:42 +0700

    Add files via upload

---
+++ flutter_application_1/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screens/Login/login_screen.dart';
+import 'package:flutter_application_1/constants.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WEMEET',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
