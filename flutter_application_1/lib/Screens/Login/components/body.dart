f085ddafae2cc6c0737431794e470207ce58d7f8
jimmy-arch <jimmywheelie19@gmail.com>
2021-03-22 16:50:43 +0700

    revisi login/register

--- flutter_application_1/lib/Screens/Login/components/body.dart
+++ flutter_application_1/lib/Screens/Login/components/body.dart
@@ -16,8 +16,6 @@
 //     Key key,
 //   }) : super(key: key);
 
-
-  
 //   @override
 //   Widget build(BuildContext context) {
 //     Size size = MediaQuery.of(context).size;
@@ -111,6 +109,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/Login/components/background.dart';
+import 'package:flutter_application_1/components/text_field_container.dart';
 import 'package:flutter_application_1/pages/root_app.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -118,96 +117,90 @@ import 'package:flutter_application_1/components/already_have_an_account_acheck.
 import 'package:flutter_application_1/Screens/Signup/signup_screen.dart';
 import 'package:flutter_application_1/components/rounded_button.dart';
 
-
 class Body extends StatefulWidget {
   @override
   _BodyState createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
-  bool visible = false ;
-    // Getting value from TextField widget.
+  bool visible = false;
+  // Getting value from TextField widget.
   final login_username = TextEditingController();
   final login_password = TextEditingController();
 
-  Future userLogin() async{
-
-  // Showing CircularProgressIndicator.
-  setState(() {
-  visible = true ; 
-  });
-
-  // Getting value from Controller
-  String email = login_username.text;
-  String password = login_password.text;
-
-  // SERVER LOGIN API URL
-  String url = 'https://wemeetuntar.000webhostapp.com/Login.php';
-
-  // Store all data with Param Name.
-  var data = {"login_username": email, "login_password" : password};
-
-  // Starting Web API Call.
-  var response = await http.post(Uri.parse(url),body: (data));
-
-  // Getting Server response into variable.
-  var message = (response.body);
-  print(response.body);
-  // If the Response Message is Matched.
-  if(message == "Login Matched")
-  {
-
-    // Hiding the CircularProgressIndicator.
-      setState(() {
-      visible = false; 
-      });
-
-    // Navigate to Profile Screen & Sending Email to Next Screen.
-      Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return RootApp();
-                    },
-                  ),
-                );
-  }
-  else{
-
-    // If Email or Password did not Matched.
-    // Hiding the CircularProgressIndicator.
+  Future userLogin() async {
+    // Showing CircularProgressIndicator.
     setState(() {
-      visible = false; 
+      visible = true;
+    });
+
+    // Getting value from Controller
+    String email = login_username.text;
+    String password = login_password.text;
+
+    // SERVER LOGIN API URL
+    String url = 'https://wemeetuntar.000webhostapp.com/Login.php';
+
+    // Store all data with Param Name.
+    var data = {"login_username": email, "login_password": password};
+
+    // Starting Web API Call.
+    var response = await http.post(Uri.parse(url), body: (data));
+
+    // Getting Server response into variable.
+    var message = (response.body);
+    print(response.body);
+    // If the Response Message is Matched.
+    if (message == "Login Matched") {
+      // Hiding the CircularProgressIndicator.
+      setState(() {
+        visible = false;
       });
 
-    // Showing Alert Dialog with Response JSON Message.
-    showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: new Text(message),
-        actions: <Widget>[
-          FlatButton(
-            child: new Text("OK"),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
+      // Navigate to Profile Screen & Sending Email to Next Screen.
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return RootApp();
+          },
+        ),
       );
-    },
-    );}
+    } else {
+      // If Email or Password did not Matched.
+      // Hiding the CircularProgressIndicator.
+      setState(() {
+        visible = false;
+      });
 
+      // Showing Alert Dialog with Response JSON Message.
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text(message),
+            actions: <Widget>[
+              FlatButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
+
   Color hexToColor(String code) {
     return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
   }
 
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-return Background(
+    return Background(
       child: Container(
         padding: new EdgeInsets.only(
             top: 20.0, right: 50.0, left: 50.0, bottom: 50.0),
@@ -225,9 +218,14 @@ return Background(
               decoration: new InputDecoration(
                 labelText: "Enter Username",
                 fillColor: Colors.white,
+                filled: true,
                 border: new OutlineInputBorder(
                   borderRadius: new BorderRadius.circular(25.0),
                   borderSide: new BorderSide(),
+                ),
+                prefixIcon: Icon(
+                  Icons.person,
+                  size: 20,
                 ),
                 //fillColor: Colors.green
               ),
@@ -243,16 +241,24 @@ return Background(
                 fontFamily: "Poppins",
               ),
             ),
+            SizedBox(
+              height: 25,
+            ),
             new TextFormField(
               controller: login_password,
-                autocorrect: true,
-                obscureText: true,
+              autocorrect: true,
+              obscureText: true,
               decoration: new InputDecoration(
                 labelText: "Enter Password",
                 fillColor: Colors.white,
+                filled: true,
                 border: new OutlineInputBorder(
                   borderRadius: new BorderRadius.circular(25.0),
                   borderSide: new BorderSide(),
+                ),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  size: 20,
                 ),
                 //fillColor: Colors.green
               ),
@@ -268,7 +274,9 @@ return Background(
                 fontFamily: "Poppins",
               ),
             ),
-            
+            SizedBox(
+              height: 15,
+            ),
             RoundedButton(
               text: "Sign in",
               press: () {
@@ -276,15 +284,11 @@ return Background(
               },
             ),
             Visibility(
-          visible: visible, 
-          child: Container(
-            margin: EdgeInsets.only(bottom: 30),
-            child: CircularProgressIndicator()
-            )
-          ),
-
+                visible: visible,
+                child: Container(
+                    margin: EdgeInsets.only(bottom: 30),
+                    child: CircularProgressIndicator())),
             SizedBox(height: size.height * 0.03),
-
             AlreadyHaveAnAccountCheck(
               press: () {
                 Navigator.push(
