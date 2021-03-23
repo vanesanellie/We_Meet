fb27cab00a07f451b8fba0f112d14c22ad576c1d
angellinaangell <70848645+angellinaangell@users.noreply.github.com>
2021-03-23 12:58:22 +0700

    edit photos chat

--- flutter_application_1/lib/constant/chat_json.dart
+++ flutter_application_1/lib/constant/chat_json.dart
@@ -1,7 +1,7 @@
 const chats = [
   {
     "id": 1,
-    "profile": "https://www.somagnews.com/wp-content/uploads/2021/03/Rose-3.jpg",
+    "profile": "https://tse2.mm.bing.net/th?id=OIP.-KWllPUC8oVtqY8p9V5wwQHaFe&pid=Api&P=0&w=247&h=184",
     "username": "Jastien Phang",
     "description": "Active",
     "dateTime": "now"
@@ -15,21 +15,21 @@ const chats = [
   },
   {
     "id": 3,
-    "profile": "https://www.somagnews.com/wp-content/uploads/2021/03/Rose-3.jpg",
+    "profile": "https://0.soompi.io/wp-content/uploads/2016/06/09174046/EXO-Chanyeol-Lucky-One1.jpg",
     "username": "Yongky Saputra",
     "description": "Active",
     "dateTime": "now"
   },
   {
     "id": 4,
-    "profile": "https://www.somagnews.com/wp-content/uploads/2021/03/Rose-3.jpg",
+    "profile": "https://tse4.mm.bing.net/th?id=OIP.VDQqUf-YiKANq6CroxOA5wHaNK&pid=Api&P=0&w=300&h=300",
     "username": "Jimmy Santoso",
     "description": "Active",
     "dateTime": "3h ago"
   },
   {
     "id": 5,
-    "profile": "https://www.somagnews.com/wp-content/uploads/2021/03/Rose-3.jpg",
+    "profile": "https://i1.wp.com/blackpinkupdate.com/wp-content/uploads/2019/02/2-BLACKPINK-Jennie-Instagram-Photo-2-February-2019-Selfie.jpg?fit=730%2C913&ssl=1",
     "username": "Vannesa Nelli",
     "description": "Active",
     "dateTime": "now"
